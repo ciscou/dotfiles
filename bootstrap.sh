530adc01c8429357d8bfc195a5b90ad37b4ec800
Francis <francismpp@gmail.com>
2026-09-23 20:22:04 +0200

    lazyvim

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,8 +11,17 @@ symlink() {
   ln -sf "$WORKDIR/$1" "$HOME/$2"
 }
 
+git_clone() {
+  echo "git cloning $1 into $2"
+  git clone "$1" "$HOME/$2"
+}
+
 if [ -d "$HOME/.config/alacritty" ]; then
   symlink "alacritty/alacritty.toml" ".config/alacritty/alacritty.toml"
+
+  if [ ! -d "$HOME/.config/alacritty/themes" ]; then
+    git_clone "https://github.com/alacritty/alacritty-theme" ".config/alacritty/themes"
+  fi
 else
   echo "Go to https://alacritty.org/#Installation to install alacritty"
 fi
@@ -21,4 +30,18 @@ if [ -d "$HOME/.config/zellij" ]; then
   symlink "zellij/config.kdl" ".config/zellij/config.kdl"
 else
   echo "Go to https://zellij.dev/documentation/installation.html#binary-download to install zellij"
+fi
+
+if [ -d "$HOME/.config/nvim" ]; then
+  # required
+  mv ~/.config/nvim{,.bak}
+
+  # optional but recommended
+  # mv ~/.local/share/nvim{,.bak}
+  # mv ~/.local/state/nvim{,.bak}
+  # mv ~/.cache/nvim{,.bak}
+
+  symlink "nvim" ".config/nvim"
+else
+  echo "Go to https://neovim.io/doc/install/ to install neovim"
 fi
