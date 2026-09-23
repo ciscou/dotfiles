a5f6a8b33c296b10cc70c9717f196992f49aec75
Francis <francismpp@gmail.com>
2026-09-23 19:04:29 +0200

    zellij

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,10 @@ WORKDIR=$(pwd)
 
 if [ -d ~/.config/alacritty ]; then
   [ -f ~/.config/alacritty/alacritty.toml ] && cp ~/.config/alacritty/alacritty.toml{,.bak}
-  echo "ln -sf $WORKDIR/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml"
   ln -sf $WORKDIR/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+fi
+
+if [ -d ~/.config/zellij ]; then
+  [ -f ~/.config/zellij/config.kdl ] && cp ~/.config/zellij/config.kdl{,.bak}
+  ln -sf $WORKDIR/zellij/config.kdl ~/.config/zellij/config.kdl
 fi
